4409026257fcf5157142d1796cde96a0b8cf0b5b
Akram-Sulthan <122307729+Akram-7869@users.noreply.github.com>
2023-05-18 13:03:27 +0530

    Add files via upload

---
+++ Car Aminated/script.js
@@ -0,0 +1,8 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src', 'sound.mp3'); 
+// audio.loop=true;
+// audio.autoplay();
+
+window.onload=function(){
+    document.getElementById("ap").onplay();
+}
